8f21ffe9e1823a317902e57eaceaaf181f573ae6
Hunter <21hcotterill@wakatipu.school.nz>
2024-05-24 15:09:07 +1200

    added pet
    pet now follows player within the detetion area and if not it idles untill player enters the area

---
+++ pet.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 70
+var player_chase = false
+var player = null
+
+func _physics_process(delta):
+	if player_chase: 
+		position += (player.position - position)/speed
+
+		$AnimatedSprite2D.play("side_bounce")
+	else: 
+		$AnimatedSprite2D.play("slime_idle")
+
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
