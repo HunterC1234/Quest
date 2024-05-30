612e499ff4eeb2f0c801cb532828f73cb291f2d9
Hunter <21hcotterill@wakatipu.school.nz>
2024-05-30 13:00:00 +1200

    Pet Debugged
    now doesn't glitch out when crossing the path

--- pet.gd
+++ pet.gd
@@ -12,7 +12,6 @@ func _physics_process(_delta):
 	else: 
 		$AnimatedSprite2D.play("slime_idle")
 
-	
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
@@ -20,6 +19,10 @@ func _on_detection_area_body_entered(body):
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
+	
+
+
+
 
 
 	
