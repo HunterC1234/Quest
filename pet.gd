a935b195163f06fe71916c30d903e8a90ced8801
Hunter <21hcotterill@wakatipu.school.nz>
2024-06-04 11:40:10 +1200

    NEW MAP
    new map added and bug fix to pet

--- pet.gd
+++ pet.gd
@@ -2,29 +2,27 @@ extends CharacterBody2D
 
 var speed = 30
 var player_chase = false
-var player = null
+var player = null 
 
-func _physics_process(_delta):
-	if player_chase: 
+
+func _physics_process(delta):
+	if player_chase:
 		position += (player.position - position)/speed
+		
+		$AnimatedSprite2D.play("run")
+		
+		if(player.position.x - position.x ) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("default")
 
-		$AnimatedSprite2D.play("side_bounce")
-	else: 
-		$AnimatedSprite2D.play("slime_idle")
-
-func _on_detection_area_body_entered(body):
+func _on_area_2d_body_entered(body):
 	player = body
 	player_chase = true
 
-func _on_detection_area_body_exited(body):
+
+func _on_area_2d_body_exited(body):
 	player = null
 	player_chase = false
-	
-
-
-
-
-
-	
-
-
