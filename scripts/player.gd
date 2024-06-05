0e02d2f5a7dd2a379bc3a77670e37d4fdc5071ae
Hunter <21hcotterill@wakatipu.school.nz>
2024-06-05 17:49:47 +1200

    8D movment
    you can now move diagonaly
    completly rechanged the coding for movment

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,70 +1,40 @@
 extends CharacterBody2D
 
-const speed = 100
-var current_dir = "none"
+var speed = 100
+
+var player_state
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta):
-	_player_movement(delta)
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
-func _player_movement(_delta):
-	if Input.is_action_pressed("ui_right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		play_anim(1)
-		velocity.x = 0
-		velocity.y = speed
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		play_anim(1)
-		velocity.x = 0
-		velocity.y = -speed
-	else:
-		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
-	
-	move_and_slide()
-
-func play_anim(movement):
-	var dir = current_dir
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
+	elif direction.x != 0 or direction.y != 0:
+		player_state = "walking"
+		
+		velocity = direction * speed
+		move_and_slide()
+		
+		play_anim(direction)
+		
+func  play_anim(direction):
 	var anim = $AnimatedSprite2D
 	
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1: 
-			anim.play("side_walk")
-		elif movement == 0:
-			anim.play("side_idle")
-	if dir == "left":
-		anim.flip_h = true
-		if movement == 1: 
-			anim.play("side_walk")
-		elif movement == 0:
-			anim.play("side_idle")
-	
-	if dir == "down":
-		anim.flip_h = true
-		if movement == 1: 
-			anim.play("front_walk")
-		elif movement == 0:
-			anim.play("front_idle")
-	if dir == "up":
-		anim.flip_h = true 
-		if movement == 1: 
-			anim.play("back_walk")
-		elif movement == 0:
-			anim.play("back_idle")
-	
+	if player_state == "idle":
+		$AnimatedSprite2D.play("front_idle")
+		
+	if player_state == "walking":
+		if direction.y == -1:
+			$AnimatedSprite2D.play("back_walk")
+		if direction.x == 1:
+			anim.flip_h = false
+			$AnimatedSprite2D.play("side_walk")
+		if direction.y == 1:
+			$AnimatedSprite2D.play("front_walk")
+		if direction.x == -1:
+			anim.flip_h = true
+			$AnimatedSprite2D.play("side_walk")
 
