aea599ae3732f41f620025b75f3b373f0fb2d5f2
Hunter <21hcotterill@wakatipu.school.nz>
2024-06-26 10:23:18 +1200

    y sort and 4d mocvment
    downgrade as it works, will experiment later

--- scenes/world.gd
+++ scenes/world.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _ready():
 	$fadein/AnimationPlayer.play("Fade_in")
-	print("called") 
+	print("fadded in...") 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,62 +1,69 @@
 extends CharacterBody2D
 
-var speed = 100
-
-var man_in_range = false
-
-var player_state
+const speed = 100
+var current_dir = "none"
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta):
-	if man_in_range == true:
-		if Input.is_action_pressed("chat"):
-			print("chatting")
-			DialogueManager.show_example_dialogue_balloon(load("res://scripts/main.dialogue"), "main")
-			return
-		
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	_player_movement(delta)
 
-	if direction.x == 0 and direction.y == 0:
-		player_state = "idle"
-	elif direction.x != 0 or direction.y != 0:
-		player_state = "walking"
-		
-		velocity = direction * speed
-		move_and_slide()
-		
-		play_anim(direction)
-		
-func  play_anim(direction):
+func _player_movement(_delta):
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = -speed
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
+
+func play_anim(movement):
+	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	
-	if player_state == "idle":
-		$AnimatedSprite2D.play("front_idle")
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1: 
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1: 
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
 	
-		
-	if player_state == "walking":
-		if direction.y == -1:
-			$AnimatedSprite2D.play("back_walk")
-		if direction.x == 1:
-			anim.flip_h = false
-			$AnimatedSprite2D.play("side_walk")
-		if direction.y == 1:
-			$AnimatedSprite2D.play("front_walk")
-		if direction.x == -1:
-			anim.flip_h = true
-			$AnimatedSprite2D.play("side_walk")
-
-var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
-
-
-
-func _on_detection_area_body_entered(body):
-	if body.has_method("old_man"):
-		man_in_range = true
-
-
-func _on_detection_area_body_exited(body):
-	if body.has_method("old_man"):
-		man_in_range = false
+	if dir == "down":
+		anim.flip_h = true
+		if movement == 1: 
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "up":
+		anim.flip_h = true 
+		if movement == 1: 
+			anim.play("back_walk")
+		elif movement == 0:
+			anim.play("back_idle")
+	
