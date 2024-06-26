e1b49605471f95f4d6a238d64b1269f1ee929dba
lynx108 <141293341+laldwell24@users.noreply.github.com>
2024-06-27 11:37:01 +1200

    NPC Dialogue
    Dialogue system fixed and now working with the old man.

--- scripts/old_man.gd
+++ scripts/old_man.gd
@@ -46,6 +46,7 @@ func _process(delta):
 			MOVE:
 				move(delta)
 	if Input.is_action_just_pressed("chat"):
+		$Dialogue.start()
 		is_roaming = false
 		is_chatting = true
 		$AnimatedSprite2D.play("idle")
@@ -61,6 +62,22 @@ func move(delta):
 
 
 
+func _on_chat_detection_area_body_entered(body):
+	if body.has_method("player"):
+		player = body
+		player_in_chat_zone = true
+
+
+func _on_chat_detection_area_body_exited(body):
+	if body.has_method("player"):
+		player_in_chat_zone = false
+
+
 func _on_timer_timeout():
 	$Timer.wait_time = choose([0.5,1,1.5])
 	current_state = choose([IDLE, NEW_DIR, MOVE])
+
+
+func _on_dialogue_dialogue_finished():
+	is_chatting = false
+	is_roaming = true
