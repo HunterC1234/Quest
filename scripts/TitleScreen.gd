fee1d9cc735aec984064156b2a05c20664eacd02
Hunter <21hcotterill@wakatipu.school.nz>
2024-08-13 11:04:58 +1200

    options menu has been created
    when starting the game the options menu is visible

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -8,13 +8,24 @@ func _on_start_button_pressed():
 	$fade_transition.show()
 	$fade_transition/Fade_timer.start()
 	$fade_transition/AnimationPlayer.play("Fade_out")
+
+func _on_options_pressed():
+	button_type = "options"
+	$fade_transition.show()
+	$fade_transition/Fade_timer.start()
+	$fade_transition/AnimationPlayer.play("Fade_out")
 	
+
 func _on_quit_pressed():
 	get_tree().quit()
  
-
 func _on_fade_timer_timeout():
 	if button_type == "start":
 		get_tree().change_scene_to_file("res://scenes/world.tscn") 
+	elif button_type == "options":
+		get_tree().change_scene_to_file("res://options.tscn")
+		
+
+
 
 
